bec1d09b37ab319de832cd72badc622b386b007f
Harshvardhan Singh <130479395+HarshvardhanSingh-13@users.noreply.github.com>
2025-05-23 17:05:33 +0530

    Add files via upload

---
+++ DA WD 9/16. Introduction to Python/main.py
@@ -0,0 +1,79 @@
+'''
+Hey This is a comment
+
+'''
+
+# print('Hey')
+# print('Yo')
+# print('He')
+
+'''
+Fundamental Datatypes
+
+Integer
+float
+String
+Boolean
+'''
+
+## Operators 
+
+# Arithmetic Operator
+
+## INT - INT
+# print(12 + 12)
+# print(25 - 5)
+# print(25 * 5)
+# print(25 / 5)
+# print(27 // 5)
+# print(27 % 5)
+# print(5 ** 3)
+
+## Bool - Bool
+
+# print(12 + True)
+
+
+# a = 25
+# b = 12
+
+# c = a % b
+# print(c)
+
+
+# name = 'Harsh'
+# age = 25
+
+# print(name +  age)
+
+
+# name = input('Give your name ')
+# age = int(input('Hi Give your age '))
+# print('Hi your name is', name, "Your age is ", age)
+
+
+## Q) Find SI
+
+P = float(input('Give P '))
+R = float(input('Give R '))
+T = int(input('Give T '))
+
+SI = (P * R * T)/100
+# print('Your SI is', SI)
+print('SI is ',(P*R*T)/100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
