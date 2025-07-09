28da096068298bc5b6e5387ef8b620fb620147d1
Harshvardhan Singh <130479395+HarshvardhanSingh-13@users.noreply.github.com>
2025-07-09 18:36:25 +0530

    Add files via upload

---
+++ DA WD 10/15. Intro to Python/main.py
@@ -0,0 +1,56 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+# print("harsh")
+# print("123")
+# print('harsh')
+
+
+'''
+int
+float
+string
+boolean
+'''
+
+
+## Operators
+
+# 1) Arithmetic OPerators
+
+## Int - INt
+
+# print(13 + 5)
+# print(13 - 5)
+# print(13 * 5)
+# print(13 / 5)
+# print(13 // 5)
+# print(13 % 5)
+# print(13 ** 5)
+
+## Int - float
+## String - String
+## Int - String
+## String - float
+## Bool - Bool
+## Int - Bool
+## Bool - String
+## Bool - Float
+
+
+#2) Logical 
+
+# print((True * True) and (True and False))
+
+# print(False or (True and False) and (True or False)) 
+
+
+# print("Harsh"+'Singh')
+
+
+
