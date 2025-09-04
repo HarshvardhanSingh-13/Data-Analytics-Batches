44ef72cb584f5a181712d1adcea01c938f1ed210
Harshvardhan Singh <130479395+HarshvardhanSingh-13@users.noreply.github.com>
2025-09-04 15:57:06 +0530

    Add files via upload

---
+++ DA WD 11/15. Intro to Python/python_basic.py
@@ -0,0 +1,76 @@
+# print('12')
+
+'''
+Data Types
+
+Integer 234,54
+String 
+Float 23.45
+Boolean 
+
+'''
+
+## Operators
+
+##1) Arithmetic Operators
+
+## INT - INT
+# print(12 + 5)
+# print(12 - 5)
+# print(12 * 5)
+# print(12 / 5)
+# print(12 // 5) 
+# print(12 % 5)
+# print(12 ** 5)
+
+## INT - Float
+## INT - BOOLEAN
+## INT - String
+## FLOAT - FLOAT
+## FLOAT - BOOLEAN
+## FLOAT - String
+## STRING - STRING
+## SRTING - BOOLEAN
+## BOOLEAN - BOOLEAN
+
+# print(type(str(5)))
+# print('Hey' + str(5))
+
+# 2) Logical Operators
+
+# 3) Comparison Operators
+
+# > < != == = 
+
+## Variables
+
+# a = 12
+# print(a + 2)
+# print(a)
+
+# first_name = int(input("Hey what is your name "))
+# print(type(first_name))
+
+## FInd Amount After SImple Interest
+
+# p= int(input("Give P "))
+# r = int(input("Give r "))
+# t = int(input("Give t "))
+
+# SI = (p*r*t)/100
+# amt = p + SI
+# print(amt)
+
+
+## Find Volume of a Cylinder
+
+pi = 3.14
+r = int(input("Give r "))
+h = int(input("Give h "))
+
+vol = pi * (r**2) * h
+print(vol)
+
+
+
+
