d044a3e7276d4afba532cef15c3946c303628feb
Harshvardhan Singh <130479395+HarshvardhanSingh-13@users.noreply.github.com>
2025-10-03 18:37:16 +0530

    Add files via upload

---
+++ DA WD 12/07. Using SELECT Clause/03. foreign key.sql
@@ -0,0 +1,28 @@
+DROP database if exists gfg;
+CREATE DATABASE gfg;
+
+CREATE TABLE IF NOT EXISTS gfg.users(
+	uid INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL);
+    
+INSERT INTO gfg.users (name, email) VALUES
+    ('John Doe', 'john.doe@example.com'),
+    ('Jane Smith', 'jane.smith@example.com'),
+    ('Bob Johnson', 'bob.johnson@example.com');
+    
+use gfg;
+-- SELECT * from users;
+CREATE TABLE IF NOT EXISTS gfg.orders(
+	oid INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    uid INT NOT NULL,
+    order_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT order_fk FOREIGN KEY(uid) REFERENCES users(uid));
+    
+INSERT INTO gfg.orders (uid)
+	VALUES
+			(1),(2);
+
+SELECT * FROM gfg.orders;
+SELECT * FROM gfg.users;
