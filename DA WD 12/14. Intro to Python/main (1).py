37e4d4b2247e0d5563dab9ac9f6ea49750db85af
Harshvardhan Singh <130479395+HarshvardhanSingh-13@users.noreply.github.com>
2025-10-29 16:06:24 +0530

    Add files via upload

---
+++ DA WD 12/14. Intro to Python/main (1).py
@@ -0,0 +1,77 @@
+# Hello
+
+# print(type("1.23"))
+# print(type("1.23"))
+# print(type("1.23"))
+# print(type(12))
+
+
+'''
+Data Types
+
+INT 233435,456
+FLOAT 23.35,4545.2
+STRING "Harsh","12"
+BOOLEAN True, False
+
+'''
+
+## Operators
+
+# 1) Arithmetic Operators
+# 2) Comprison Operators (<,>,=,==)
+
+## INT - INT
+# print(12 + 5)
+# print(12 - 5)
+# print(12 * 5)
+# print(12 / 5)
+# print(12 // 5) ## Floor Divsion
+# print(12 % 5) ## Modulus Operators
+# print(12 ** 5) ## Exponential
+
+## INT - FLOAT
+## FLOAT - FLOAT
+## STRING - STRING
+
+# print("ABC"+"DEF")
+
+## STRING - INT
+# print("ABC"*5)
+## STRING - BOOL
+## BOOL - BOOL
+# print(True % True)
+
+# print("ABC" + 12)
+# print("ABC" + str(12))
+
+# print(type(str(12)))
+# print(type(int("12")))
+
+# int()
+# str()
+# float()
+# bool()
+
+# print(int("123"))
+
+## Variables
+
+
+# a = 12 + 5
+# b = 12 -5
+
+# print(a * b)
+
+
+_aw_12 = 1
+print(_aw_12)
+
+
+
+
+
+
+
+
+
